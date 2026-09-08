822c3820511a34c449b9440adf5ddeb19f90c38d
gameai2027-gif <gameai2027@gmail.com>
2026-09-08 14:19:54 +0500

    feat: homepage structure + conversion/SEO updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
+import { StatsBar } from "@/components/StatsBar";
+import { ProblemSolution } from "@/components/ProblemSolution";
 import { TrustBar } from "@/components/TrustBar";
 import { Advantages } from "@/components/Advantages";
 import { PopularCars } from "@/components/PopularCars";
@@ -7,61 +9,67 @@ import { Calculator } from "@/components/Calculator";
 import { HowItWorks } from "@/components/HowItWorks";
 import { Reviews } from "@/components/Reviews";
 import { WhyUs } from "@/components/WhyUs";
+import { ServiceArea } from "@/components/ServiceArea";
 import { LeadForm } from "@/components/LeadForm";
 import { Footer } from "@/components/Footer";
+import { SITE } from "@/lib/site";
 
 export default function HomePage() {
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "AutoDealer",
+    name: SITE.name,
+    alternateName: SITE.shortName,
+    description:
+      "Импорт автомобилей из Китая, Кореи и Японии с доставкой в Уфу и Республику Башкортостан",
+    url: "https://aybaza.ru",
+    telephone: SITE.phoneHref.replace("tel:", ""),
+    email: SITE.email,
+    address: {
+      "@type": "PostalAddress",
+      streetAddress: SITE.address.street,
+      addressLocality: SITE.address.city,
+      addressRegion: SITE.address.region,
+      postalCode: SITE.address.postalCode,
+      addressCountry: "RU",
+    },
+    geo: {
+      "@type": "GeoCoordinates",
+      latitude: SITE.geo.lat,
+      longitude: SITE.geo.lng,
+    },
+    openingHoursSpecification: {
+      "@type": "OpeningHoursSpecification",
+      dayOfWeek: [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+      ],
+      opens: "09:00",
+      closes: "21:00",
+    },
+    areaServed: SITE.serviceArea.map((city) => ({
+      "@type": "City",
+      name: city,
+    })),
+  };
+
   return (
     <>
       <script
         type="application/ld+json"
-        dangerouslySetInnerHTML={{
-          __html: JSON.stringify({
-            "@context": "https://schema.org",
-            "@type": "AutoDealer",
-            name: "ООО АЙБАЗА",
-            description:
-              "Импорт автомобилей из Китая, Кореи и Японии с доставкой в Уфу и Башкортостан",
-            url: "https://aybaza.ru",
-            telephone: "+7-347-200-00-00",
-            address: {
-              "@type": "PostalAddress",
-              streetAddress: "ул. Ленина, 45",
-              addressLocality: "Уфа",
-              addressRegion: "Республика Башкортостан",
-              postalCode: "450000",
-              addressCountry: "RU",
-            },
-            geo: {
-              "@type": "GeoCoordinates",
-              latitude: 54.7351,
-              longitude: 55.9587,
-            },
-            openingHoursSpecification: {
-              "@type": "OpeningHoursSpecification",
-              dayOfWeek: [
-                "Monday",
-                "Tuesday",
-                "Wednesday",
-                "Thursday",
-                "Friday",
-                "Saturday",
-                "Sunday",
-              ],
-              opens: "09:00",
-              closes: "21:00",
-            },
-            areaServed: {
-              "@type": "AdministrativeArea",
-              name: "Республика Башкортостан",
-            },
-          }),
-        }}
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
       />
 
       <Header />
       <main>
         <Hero />
+        <StatsBar />
+        <ProblemSolution />
         <TrustBar />
         <Advantages />
         <PopularCars />
@@ -69,6 +77,7 @@ export default function HomePage() {
         <HowItWorks />
         <Reviews />
         <WhyUs />
+        <ServiceArea />
         <LeadForm />
       </main>
       <Footer />
